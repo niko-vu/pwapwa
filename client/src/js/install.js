0d675e1649ad3006964ba11cc0977e30f54f04e6
fearoffun <480dunce@gmail.com>
2023-09-03 15:47:29 -0700

    install page done

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,14 +1,26 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
+let deferredPrompt;
+
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
     event.preventDefault();
-    const jate = form.elements['jate'].value;
+    deferredPrompt = event;
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    hideInstallPromotion();
+    deferredPrompt.prompt();
+    const { outcome } = await deferredPrompt.userChoice;
+    console.log(`User response to the install prompt: ${outcome}`);
+    deferredPrompt = null;
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    hideInstallPromotion();
+    deferredPrompt = null;
+    console.log('PWA was installed! Have fun. :)');
+});
