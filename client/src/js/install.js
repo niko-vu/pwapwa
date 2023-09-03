36381e6e7de5ab70395159a2f3617cf7363c0bef
fearoffun <480dunce@gmail.com>
2023-09-03 16:02:17 -0700

    fixed install js file

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -5,22 +5,46 @@ let deferredPrompt;
 
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
+    console.log("beforeinstallprompt fired");
     event.preventDefault();
     deferredPrompt = event;
+    installButton.hidden = false;
+    installButton.addEventListener("click", installApp);
 });
+
+function installApp() {
+    deferredPrompt.prompt();
+    installButton.disabled = true;
+
+    deferredPrompt.userChoice.then(choiceResult => {
+        if (choiceResult.outcome === "accepted") {
+          console.log("PWA setup accepted");
+          installButton.hidden = true;
+        } else {
+          console.log("PWA setup rejected");
+        }
+        installButton.disabled = false;
+        deferredPrompt = null;
+      });
+}
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
-    hideInstallPromotion();
-    deferredPrompt.prompt();
-    const { outcome } = await deferredPrompt.userChoice;
-    console.log(`User response to the install prompt: ${outcome}`);
-    deferredPrompt = null;
+    // hideInstallPromotion();
+    if (!installPrompt) {
+        return;
+    }
+    const result = await installPrompt.prompt();
+    console.log(`Install prompt was: ${result.outcome}`);
+    installPromptPoof();
 });
+
+function installPromptPoof() {
+    installPrompt = null;
+    installButton.setAttribute('hidden', '');
+}
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
-    hideInstallPromotion();
-    deferredPrompt = null;
-    console.log('PWA was installed! Have fun. :)');
+    installPromptPoof();
 });
